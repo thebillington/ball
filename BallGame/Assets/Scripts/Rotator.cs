dca754afb51789957b63fcb7a58c08ecc361c302
Billy Rebecchi <thebillington@Billys-MacBook-Pro.local>
2015-11-20 23:10:52 +0000

    Add the collectible game objects and give them a colour.

---
+++ BallGame/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Rotates by a new eulerVector, and times the result by the Delta time to make the object frame rate independent
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+
+	}
+}
