02830b9f7e99dc85304410c412f5a1734bae6b0a
Billy Rebecchi <thebillington@Billys-MacBook-Pro.local>
2015-11-20 22:38:22 +0000

    Add walls.

---
+++ BallGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Late Update is called after everything in update has been called
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
