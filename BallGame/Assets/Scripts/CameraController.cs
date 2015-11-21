f1012609c12e40c5775e94db6f20a71a2cbad769
Billy Rebecchi <thebillington@Billys-MacBook-Pro.local>
2015-11-21 01:19:32 +0000

    Make camera rotate around object following mouse.

--- BallGame/Assets/Scripts/CameraController.cs
+++ BallGame/Assets/Scripts/CameraController.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
+	
+	public float turnSpeed = 4.0f;
 
 	public GameObject player;
 
@@ -14,6 +16,8 @@ public class CameraController : MonoBehaviour {
 	
 	// Late Update is called after everything in update has been called
 	void LateUpdate () {
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
 		transform.position = player.transform.position + offset;
+		transform.LookAt (player.transform.position);
 	}
 }
